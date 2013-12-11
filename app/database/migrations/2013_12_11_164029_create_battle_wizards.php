c10b303c1d90e688449f210df57172b50edc2adc
ilya.rubinchik <ilya.rubinchik@home24.de>
2013-12-11 18:02:11 +0100

    battle wizards table

---
+++ app/database/migrations/2013_12_11_164029_create_battle_wizards.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateChunks extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('battle_wizards', function(Blueprint $table) {
+			$table->integer('id');
+			$table->integer('battle_map_id')->unsigned()->index();
+			$table->integer('wizard_id')->unsigned();
+			$table->integer('user_id')->unsigned();
+			$table->integer('chunk_id')->unsigned();
+			$table->timestamp('start');
+            $table->text('data');
+
+            $table->foreign('battle_map_id')->references('id')->on('battle_maps')->onDelete('cascade');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('chunks');
+	}
+
+}
